44a51627f65dbd4f595e00ccdc8672c9a13d3466
Mukhammad Khalimudin <khalimudin@gmail.com>
2025-11-22 22:14:14 +0700

    chapter 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+
+// You can add more font exports here as needed
